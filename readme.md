5b53a6ad1d98731e2f0e00d477ba99f82b7160f1
Aditya Sharma <aditya.sharma9301@gmail.com>
2021-04-03 12:48:56 +0530

    added tabs to webpage

---
+++ readme.md
@@ -0,0 +1,2 @@
+## Groww.in clone
+A simple groww landing page clone made using html,css and js.
--- src/script.js
+++ src/script.js
@@ -1,7 +1,29 @@
-var tablinks = document.getElementsByClassName("tab-section");
-for (let i=0 ; i<tablinks.length ;i++) {
-    tablinks[i].className = tablinks[i].className.replace("active" , "");
-}
+// var default_selected = document.getElementById('stocks');
+// document.getElementsByClassName('tab-links')[0].click(
+//     // document.getElementById('stocks').style.display = block;
+// );
+
+// function open_section(evt, section_name) {
+//   var i, tabcontent, tablinks;
+    
+//     tabcontent = document.getElementsByClassName("tab-content");
+//     for (i = 0; i < tabcontent.length; i++) {
+//         tabcontent[i].style.display = "none";
+
+//     }
+
+//   tablinks = document.getElementsByClassName("tab-links");
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" active", "");
+//     break;
+//   }
+//   document.getElementById(section_name).style.display = "block";
+//   evt.currentTarget.className += " active";
+// }
+
+// var default = document.getElementsByClassName('tablinks')[0];
+// document.getElementById(default).style.display = "block";
+
 
 function open_section(evt, section_name) {
   var i, tabcontent, tablinks;
@@ -9,10 +31,12 @@ function open_section(evt, section_name) {
   tabcontent = document.getElementsByClassName("tab-content");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
+    console.log(i);
   }
   tablinks = document.getElementsByClassName("tab-links");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
+    console.log("hi" + i);
   }
   document.getElementById(section_name).style.display = "block";
   evt.currentTarget.className += " active";
